22f684296bed375dba3ba20636a2ac5ba23d5873
Rodrj1 <rodrigo_mdq93@hotmail.com>
2023-01-21 03:53:02 -0300

    Deleting files

--- src/data/exercises.ts
+++
@@ -1,6 +0,0 @@
-export const exercises = [
-  { ARMS: ["Biceps Curl", "Triceps Curl"] },
-  { CHEST: ["Barbell Press", "Bench Press Declined"] },
-  { LEGS: ["Squat", "Deadlift"] },
-  { BACK: ["Chin Ups"] },
-];
